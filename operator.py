a926da3b5f2d777ebaff1c1c5dd451898e5ea5b1
lukasbaumgartner <lukasfbaumgartner@gmail.com>
2026-01-27 16:23:51 +0100

    Session 5 work

---
+++ operator.py
@@ -0,0 +1,26 @@
+# Arithmetic operators ( +, -, etc)
+print("2+4=", 2+4)
+print("6.4-4.2=", 6.4-4.2)
+print("9*9", 9*9)
+
+print("64/8=", 64/8)
+# modula, % gives the remainder, does not divide, but if divided how much of the values would be left
+print("9%2=", 9%2)
+# 1
+print("99%100", 99%100)
+# 99
+
+
+# integer result for division requires two backslackes
+print("8/2 =", 8//2)
+
+# to the power of is **
+print("3^4= ", 3**4)
+
+# absolute values
+print(abs(-1))
+
+# To print as an integer.
+print(int("123"))
+
+# Boolean: True or False
