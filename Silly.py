c6e116a2b308fe4deb4675438bce52fef55b2d6a
lukasbaumgartner <lukasfbaumgartner@gmail.com>
2026-01-27 17:12:22 +0100

    Re-organize

---
+++ Silly.py
@@ -0,0 +1,7 @@
+
+True
+# Orange, you cant touch it, blue you can
+bob = print
+print = "Bob is big"
+bob("hello world")
+bob(print)
--- input.py
+++ input.py
@@ -1,12 +1,2 @@
-
-True
-# Orange, you cant touch it, blue you can
-bob = print
-print = "Bob is big"
-bob("hello world")
-bob(print)
-
-# above was silly learning now we do class example\
-
 name = input("What is your name? ")
 print("nice to meet you", name)
